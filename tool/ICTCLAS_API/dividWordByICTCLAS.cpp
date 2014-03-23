7a439f76710986c96dde67c6b37f8e30755fe171
HCCChen <ccx97u@gmail.com>
2014-03-23 15:46:46 +0800

    Add Word Segmentation and POS-TAG Merge

--- tool/ICTCLAS_API/dividWordByICTCLAS.cpp
+++ tool/ICTCLAS_API/dividWordByICTCLAS.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include  <string>
 #include "ICTCLAS50.h"
+#include "../library.h"
 
 using namespace std;
 
@@ -18,14 +19,125 @@ string divideSentence(string sentence);
 int main(int argc, char* argv[])
 {
     char* sResult;
+	const string DIR_PATH = "../../featureCreate/data/preProcessLaw/";
+	vector<string> files = vector<string>();
+	vector<string> lawSeg;
+	fstream fin, fout;
+	char buf[4096];
+	string tmpStr, filePath, lawSentence;
+	int i,j,k, pos1, pos2;
+	char posFlag = 'N';
+
 	if(!ICTCLAS_Init())			  
 	{	printf("Init fails\n");
 		return -1;	}
-	if(argc == 2){
-		string sParagraph = argv[1] ;
-		cout << divideSentence(sParagraph) << endl;
-		ICTCLAS_Exit();
+
+	getdir(DIR_PATH, files);
+	for(i = 0; i < files.size(); i++){//For each Law
+		if(files[i].length() < 3){continue;}
+		filePath = DIR_PATH + files[i];
+		fin.open(filePath.c_str(), ios::in);
+		while(!fin.eof()){
+			lawSeg.clear();
+			fin.getline(buf, 4096);
+			if(strlen(buf) < 2){break;}
+			lawSentence.assign(buf);
+			lawSentence = divideSentence(lawSentence);
+			cout << lawSentence << endl;
+			//Divide and get pos-tsg
+			explode(' ', lawSentence, lawSeg);
+			for(j = 0, posFlag = 'N'; j < lawSeg.size(); j++){//For each word
+				if(posFlag == 'N'){//POS-merge Begin
+					if(lawSeg[j].find("/m") != string::npos){
+						posFlag = 'm';
+						pos1 = j;
+					}
+					else if(lawSeg[j].find("/v") != string::npos){
+						posFlag = 'v';
+						pos1 = j;
+					}
+					else if(lawSeg[j].find("/a") != string::npos 
+						|| lawSeg[j].find("/x") != string::npos){
+						posFlag = 'a';
+						pos1 = j;
+						pos2 = j;
+					}
+					else if(lawSeg[j].find("/d") != string::npos){ 
+						posFlag = 'd';
+						pos1 = j;
+						pos2 = j;
+					}
+
+				}
+				else if(posFlag == 'm'){
+					if(lawSeg[j].find("/m") != string::npos){//m+....m
+						continue;
+					}
+					else if(lawSeg[j].find("/q") != string::npos || lawSeg[j].find("/f") != string::npos){//m+....+q
+						//Merge it!!
+						posFlag = 'N';
+					}
+					else{//Undefine Merge
+						posFlag = 'N';
+					}
+				}
+				else if(posFlag == 'v'){
+					if(lawSeg[j].find("/n") != string::npos){//v+n
+						//Merge it!!
+						posFlag = 'N';
+					}
+					else{
+						posFlag = 'N';
+					}
+				}
+				else if(posFlag == 'a'){
+					if(lawSeg[j].find("/n") != string::npos //a+n+...
+					|| lawSeg[j].find("/vn") != string::npos //a+vn+...
+					|| lawSeg[j].find("/a") != string::npos){//a+a+...
+						pos2 = j;
+						continue;
+					}
+					else{
+						if(pos1 != pos2){
+							//Merge it!!
+							cout << "\t";
+							for(k = pos1; k < j; k++){
+								cout << lawSeg[k].substr(0, lawSeg[k].find('/'));
+							}
+							cout << endl;
+							posFlag = 'N';	
+						}
+						posFlag = 'N';
+					}
+				}
+				else if(posFlag == 'd'){
+					if(lawSeg[j].find("/a") != string::npos //d+a
+					|| lawSeg[j].find("/n") != string::npos //d+n
+					|| lawSeg[j].find("/x") != string::npos){//d+x
+						pos2 = j;
+						continue;
+					}
+					else{
+						if(lawSeg[j].find("/n") != string::npos//d+...+n
+						|| lawSeg[j].find("/vn") != string::npos//d+...+vn
+						|| lawSeg[j].find("/q") != string::npos){pos2++;}//d+...+q
+						if(pos1 != pos2){
+							cout << "\t";
+							for(k = pos1; k < j; k++){
+								cout << lawSeg[k].substr(0, lawSeg[k].find('/'));
+							}
+							cout << endl;
+							posFlag = 'N';	
+						}
+						posFlag = 'N';
+					}
+				}
+
+			}
+		}
+		fin.close();
 	}
+	ICTCLAS_Exit();
 	return 0;
 }
 
