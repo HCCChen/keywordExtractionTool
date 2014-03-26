a418657c089b281eebf90f8e7300c6044677aaa1
HCCChen <ccx97u@gmail.com>
2014-03-26 15:17:50 +0800

    Add "strReplaceAll" Function

--- tool/library.cpp
+++ tool/library.cpp
@@ -31,3 +31,42 @@ int countFileLine(string filePath){
 	return count;
 }
 
+int explode(char divideChar, string originalString, vector<string> &stringAry){
+	int flag = 0, preFlag = 0 ,index = 0;
+	int length = originalString.length();
+	string tmpStr;
+	stringAry.clear();
+	while(flag = originalString.find(divideChar, flag)){
+		tmpStr = originalString.substr(preFlag, flag-preFlag);
+		stringAry.push_back(tmpStr);
+		index++;
+		if(flag > length || flag < 0){return index;}
+		flag++;
+		preFlag = flag;
+	}
+	return -1;
+}
+
+
+string int2str(int &i) {
+	string s;
+	stringstream ss(s);
+	ss << i;
+	return ss.str();
+}
+
+int str2int(string str){
+	return atoi(str.c_str());
+}
+
+string strReplaceAll(string sources, string originStr, string targetStr){
+	string tmpStr1, tmpStr2;
+	int i, j, pos;
+	while(sources.find(originStr) != string::npos){
+		pos = sources.find(originStr);
+		tmpStr1 = sources.substr(0, pos);
+		tmpStr2 = sources.substr(pos+originStr.length());
+		sources = tmpStr1 + targetStr + tmpStr2;
+	}
+	return sources;
+}
--- tool/library.h
+++ tool/library.h
@@ -4,17 +4,31 @@
 	Target: 
 */
 #include <iostream>
+#include <cstdlib>
+#include <string>
 #include <fstream>
-#include <vector>
+#include <sstream>
 #include <map>
-#include <cstring>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <regex.h>
 #include <sys/types.h>
 #include <dirent.h>
-#include <scws.h>
 #include <errno.h>
+#include <scws.h>
 
 using namespace std;
 
+//Get all file name in director
 int getdir(string dir, vector<string> &files);
+//Count the total line in file
 int countFileLine(string filePath);
-
+//integer to string
+string int2str(int &i);
+//integer to string
+int str2int(string str);
+//Divide Sentence base on flag character
+int explode(char divideChar, string originalString, vector<string> &stringAry);
+//Replace all sub_string
+string strReplaceAll(string sources, string originStr, string targetStr);
