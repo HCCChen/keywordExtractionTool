8c622a0f37593d9a96e46cacd1465ab1e2f5353a
HCCChen <ccx97u@gmail.com>
2014-06-24 15:53:39 +0800

    BACKUP

--- tool/svmPreprocess.cpp
+++ tool/svmPreprocess.cpp
@@ -11,8 +11,13 @@ using namespace std;
 int main(int argc, char* argv[]){
 	const string POS_FILE_PATH = "../featureCreate/trainingPosBase";
 	const string NEG_FILE_PATH = "../featureCreate/trainingNegBase";
+	const string TEST_FILE_PATH = "../featureCreate/testingBase";
+	/*
 	const string TRAIN_DATA_PATH = "../libsvm-3.17/trainingData";
 	const string TRAIN_WORD_PATH = "../libsvm-3.17/trainingKeyword";
+	*/
+	const string TRAIN_DATA_PATH = "../libsvm-3.17/testingData";
+	const string TRAIN_WORD_PATH = "../libsvm-3.17/testingKeyword";
 	fstream fin, fout1, fout2;
 	char buf[4096];
 	string tmpStr, rowData[2];
@@ -21,6 +26,7 @@ int main(int argc, char* argv[]){
 	fout1.open(TRAIN_DATA_PATH.c_str(), ios::out);
 	fout2.open(TRAIN_WORD_PATH.c_str(), ios::out);
 	//Load file
+	/*
 	fin.open(POS_FILE_PATH.c_str(), ios::in);
 	while(!fin.eof()){
 		fin.getline(buf, 4096);
@@ -43,8 +49,21 @@ int main(int argc, char* argv[]){
 		fout1 << rowData[1] << endl;
 		fout2 << rowData[0] << endl;
 	}
-
+	fin.close();
+	*/
+	fin.open(TEST_FILE_PATH.c_str(), ios::in);
+	while(!fin.eof()){
+		fin.getline(buf, 4096);
+		tmpStr.assign(buf);
+		rowData[0] = tmpStr.substr(0, tmpStr.find('\t'));
+		rowData[1] = tmpStr.substr(tmpStr.find('\t')+1);
+		rowData[1][0] = '-';
+		if(rowData[0].length() < 3){continue;}
+		fout1 << rowData[1] << endl;
+		fout2 << rowData[0] << endl;
+	}
 	fin.close();	
+
 	fout1.close();
 	fout2.close();
 	
