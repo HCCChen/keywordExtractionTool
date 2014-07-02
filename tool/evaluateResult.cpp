dc58f789e623ab103033100e621ceccd847ca681
HCCChen <ccx97u@gmail.com>
2014-07-02 20:38:29 +0800

    Add precision/recall/f-measure summerize

--- tool/evaluateResult.cpp
+++ tool/evaluateResult.cpp
@@ -8,15 +8,21 @@
 #include <vector>
 #include <fstream>
 #include <cstdlib>
+#include "library.h"
 using namespace std;
 
 int main(int argc, char* argv[]){
 	const string SOURCE_DATA_PATH = "../libsvm-3.17/experimentRecord";
 	const string RESULT_DATA_PATH = "../featureCreate/data/experimentResult";
+	const string SOURCE_DATA_PATH2 = "../libsvm-3.17/statisticRecord";
+	const string RESULT_DATA_PATH2 = "../featureCreate/data/statisticResult";
 	char buf[4096];
 	string tmpStr, strSeq;
 	fstream fin, fout;
 	int data1 = 0, data2 = 0, pos1, pos2, flag = 0;
+	int lineFlag = 0, cycleFlag = 0;
+	double accruacy, precision, recall, fmeasure;
+	double sumOfAccuracy = 0, sumOfPrecision = 0, sumOfRecall = 0, sumOfFmeasure = 0;
 
 	fin.open(SOURCE_DATA_PATH.c_str(), ios::in);
 	fout.open(RESULT_DATA_PATH.c_str(), ios::out);
@@ -41,14 +47,40 @@ int main(int argc, char* argv[]){
 			data1 += atoi(tmpStr.c_str());
 			tmpStr = strSeq.substr(pos1+1);
 			data2 += atoi(tmpStr.c_str());
-			
-		
 		}
 	}
 	fout << "Accuracy = " <<((double)(data1)/(double)(data2))*100 << "% (" << data1 << "/" << data2 << ") (classification)" << endl;
 
 	fin.close();
 	fout.close();
-	
+
+
+	fin.open(SOURCE_DATA_PATH2.c_str(), ios::in);
+	fout.open(RESULT_DATA_PATH2.c_str(), ios::out);
+	lineFlag = 0;
+	while(!fin.eof()){
+		fin.getline(buf, 4096);
+		tmpStr.assign(buf);
+		if(lineFlag % 3 == 0){accruacy = atof(tmpStr.c_str());}
+		else if (lineFlag % 3 == 1){precision = atof(tmpStr.c_str());}
+		else{
+			recall = atof(tmpStr.c_str());
+			fmeasure = (2*precision*recall)/(precision+recall);
+
+			sumOfRecall += recall;
+			sumOfAccuracy += accruacy;
+			sumOfPrecision += precision;
+			sumOfFmeasure += fmeasure;
+			cycleFlag++;
+		}
+		lineFlag++;
+	}
+	fout << "Accuracy: " << sumOfAccuracy/cycleFlag << endl;
+	fout << "Precision: " << sumOfPrecision/cycleFlag << endl;
+	fout << "Recall: " << sumOfRecall/cycleFlag << endl;
+	fout << "F-measures: " << sumOfFmeasure/cycleFlag << endl;
+	fin.close();
+	fout.close();
+
 	return 0;
 }
